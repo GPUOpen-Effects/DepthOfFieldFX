5ffcef4657a4f5f2f0dd8f21beaf8fdd8ad38f42
Jason Stewart <Jason.Stewart@amd.com>
2017-03-28 11:45:11 -0400

    Squashed 'framework/d3d11/amd_sdk/' changes from acde11c..7bc7285
    7bc7285 Update build scripts for newer version of premake
    
    git-subtree-dir: framework/d3d11/amd_sdk
    git-subtree-split: 7bc7285260e89475ac7429234ed681e20c410a49

--- premake/premake5.lua
+++ premake/premake5.lua
@@ -14,6 +14,7 @@ workspace "AMD_SDK"
 project "AMD_SDK"
    kind "StaticLib"
    language "C++"
+   characterset "Unicode"
    location "../build"
    filename ("AMD_SDK" .. _AMD_VS_SUFFIX)
    uuid "EBB939DC-98E4-49DF-B1F1-D2E80A11F60A"
@@ -21,20 +22,21 @@ project "AMD_SDK"
    objdir "../build/%{_AMD_SAMPLE_DIR_LAYOUT}"
    warnings "Extra"
    floatingpoint "Fast"
+   symbols "On"
 
    -- Specify WindowsTargetPlatformVersion here for VS2015
-   windowstarget (_AMD_WIN_SDK_VERSION)
+   systemversion (_AMD_WIN_SDK_VERSION)
 
    files { "../inc/**.h", "../src/**.h", "../src/**.cpp", "../src/**.hlsl" }
    includedirs { "../../dxut/Core", "../../dxut/Optional", "../../../../third_party/assimp/include" }
 
    filter "configurations:Debug"
       defines { "WIN32", "_DEBUG", "DEBUG", "PROFILE", "_WINDOWS", "_LIB", "_WIN32_WINNT=0x0601" }
-      flags { "Symbols", "FatalWarnings", "Unicode" }
+      flags { "FatalWarnings" }
       targetsuffix ("_Debug" .. _AMD_VS_SUFFIX)
 
    filter "configurations:Release"
       defines { "WIN32", "NDEBUG", "_WINDOWS", "_LIB", "_WIN32_WINNT=0x0601" }
-      flags { "LinkTimeOptimization", "Symbols", "FatalWarnings", "Unicode" }
+      flags { "LinkTimeOptimization", "FatalWarnings" }
       targetsuffix ("_Release" .. _AMD_VS_SUFFIX)
       optimize "On"
