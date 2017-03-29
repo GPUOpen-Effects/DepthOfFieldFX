76f40220852c3b35f73b1cb73cbe941a9f8383e1
Jason Stewart <jstewart-amd@users.noreply.github.com>
2017-03-29 13:09:32 -0400

    Drop support for VS2012

--- premake/amd_premake_util.lua
+++ premake/amd_premake_util.lua
@@ -1,18 +1,18 @@
 -- amd_premake_util.lua
 -- utility code shared by AMD build scripts
 
--- _ACTION is a premake global variable and for our usage will be vs2012, vs2013, etc.
+-- _ACTION is a premake global variable and for our usage will be vs2013, vs2015, etc.
 -- Strip "vs" from this string to make a suffix for solution and project files.
 _AMD_VS_SUFFIX = "_" .. string.gsub(_ACTION, "vs", "")
 
--- _ACTION is a premake global variable and for our usage will be vs2012, vs2013, etc.
+-- _ACTION is a premake global variable and for our usage will be vs2013, vs2015, etc.
 -- Make an allcaps version. We use this for some directory names.
 _AMD_ACTION_ALL_CAPS = string.upper(_ACTION)
 
 -- Specify the DLL prefix here
 _AMD_LIBRARY_PREFIX = "GPUOpen_"
 
--- Specify build output directory structure here: e.g. VS2012\x64\DLL_Debug
+-- Specify build output directory structure here: e.g. VS2015\x64\DLL_Debug
 _AMD_SAMPLE_DIR_LAYOUT  = "Desktop%{_AMD_VS_SUFFIX}/%{cfg.platform}/%{cfg.buildcfg}"
 _AMD_LIBRARY_DIR_LAYOUT = "%{_AMD_ACTION_ALL_CAPS}/%{cfg.platform}/%{cfg.buildcfg}"
 _AMD_LIBRARY_DIR_LAYOUT_BACKSLASH = "%{_AMD_ACTION_ALL_CAPS}\\%{cfg.platform}\\%{cfg.buildcfg}"
@@ -26,7 +26,6 @@ _AMD_LIBRARY_DIR_LAYOUT_MINIMAL = "%{_AMD_ACTION_ALL_CAPS}/minimal/%{cfg.platfor
 _AMD_WIN_SDK_VERSION = "8.1"
 
 -- command lines for Visual Studio build events
-_AMD_COPY_WIN_8_0_SDK_REDIST_TO_BIN = "if not exist \"..\\bin\\d3dcompiler_46.dll\" if exist \"$(ProgramFiles)\\Windows Kits\\8.0\\Redist\\D3D\\x64\\d3dcompiler_46.dll\" xcopy \"$(ProgramFiles)\\Windows Kits\\8.0\\Redist\\D3D\\x64\\d3dcompiler_46.dll\" \"..\\bin\" /H /R /Y > nul"
 _AMD_COPY_WIN_8_1_SDK_REDIST_TO_BIN = "if not exist \"..\\bin\\d3dcompiler_47.dll\" if exist \"$(ProgramFiles)\\Windows Kits\\8.1\\Redist\\D3D\\x64\\d3dcompiler_47.dll\" xcopy \"$(ProgramFiles)\\Windows Kits\\8.1\\Redist\\D3D\\x64\\d3dcompiler_47.dll\" \"..\\bin\" /H /R /Y > nul"
 _AMD_COPY_AGS_RLS_DLL_TO_BIN = "xcopy \"..\\..\\amd_lib\\ags_lib\\lib\\amd_ags_x64.dll\"  \"..\\bin\" /H /R /Y > nul"
 _AMD_COPY_ASSIMP_RLS_DLL_TO_BIN = "xcopy \"..\\..\\third_party\\assimp\\lib\\assimp_x64%{_AMD_VS_SUFFIX}.dll\"  \"..\\bin\" /H /R /Y > nul"
@@ -43,10 +42,6 @@ function amdSamplePostbuildCommands(copyAgs, copyAssimp)
    local commands = {}
    local doCopyAgs = copyAgs or false
    local doCopyAssimp = copyAssimp or false
-   -- for VS2012 and earlier, copy d3dcompiler_46.dll from the 8.0 SDK to the local bin directory
-   if _ACTION <= "vs2012" then
-      table.insert(commands, _AMD_COPY_WIN_8_0_SDK_REDIST_TO_BIN)
-   end
    -- copy d3dcompiler_47.dll from the 8.1 SDK to the local bin directory
    table.insert(commands, _AMD_COPY_WIN_8_1_SDK_REDIST_TO_BIN)
    -- copy the library DLLs to the local bin directory
--- premake/gpuopen_fx_update_vs_files.bat
+++ premake/gpuopen_fx_update_vs_files.bat
@@ -12,21 +12,21 @@ cd "%~dp0"
 
 echo --- amd_lib ---
 cd ..\amd_lib\shared\d3d11\premake
-call :createvsfilesincluding2010
+call :createvsfilesforsharedcode
 call :createvsfileswithminimaldependencies
 
 echo --- amd_sdk ---
 cd ..\..\..\..\framework\d3d11\amd_sdk\premake
-call :createvsfilesincluding2010
+call :createvsfilesforsharedcode
 call :createvsfileswithminimaldependencies
 
 echo --- dxut core ---
 cd ..\..\dxut\Core
-call :createvsfilesincluding2010
+call :createvsfilesforsharedcode
 
 echo --- dxut optional ---
 cd ..\Optional
-call :createvsfilesincluding2010
+call :createvsfilesforsharedcode
 cd ..\..\..\..\
 :: we don't keep solution files for amd_lib, amd_sdk, or dxut
 call :cleanslnfiles
@@ -57,58 +57,41 @@ if exist %1\premake (
 )
 goto :EOF
 
-:: run premake for vs2012, vs2013, and vs2015
+:: run premake for vs2013 and vs2015
 :createvsfiles
-..\..\premake\premake5.exe vs2012
 ..\..\premake\premake5.exe vs2013
 ..\..\premake\premake5.exe vs2015
 goto :EOF
 
-:: run premake for vs2010, vs2012, vs2013, and vs2015
-:createvsfilesincluding2010
-..\..\..\..\premake\premake5.exe vs2010
-..\..\..\..\premake\premake5.exe vs2012
+:: run premake for vs2013 and vs2015
+:createvsfilesforsharedcode
 ..\..\..\..\premake\premake5.exe vs2013
 ..\..\..\..\premake\premake5.exe vs2015
 goto :EOF
 
-:: run premake for vs2010, vs2012, vs2013, and vs2015
+:: run premake for vs2013 and vs2015
 :createvsfileswithminimaldependencies
-..\..\..\..\premake\premake5.exe --file=premake5_minimal.lua vs2010
-..\..\..\..\premake\premake5.exe --file=premake5_minimal.lua vs2012
 ..\..\..\..\premake\premake5.exe --file=premake5_minimal.lua vs2013
 ..\..\..\..\premake\premake5.exe --file=premake5_minimal.lua vs2015
 goto :EOF
 
 :: delete unnecessary sln files
 :cleanslnfiles
-del /f /q amd_lib\shared\d3d11\build\AMD_LIB_2010.sln
-del /f /q amd_lib\shared\d3d11\build\AMD_LIB_2012.sln
 del /f /q amd_lib\shared\d3d11\build\AMD_LIB_2013.sln
 del /f /q amd_lib\shared\d3d11\build\AMD_LIB_2015.sln
 
-del /f /q amd_lib\shared\d3d11\build\AMD_LIB_Minimal_2010.sln
-del /f /q amd_lib\shared\d3d11\build\AMD_LIB_Minimal_2012.sln
 del /f /q amd_lib\shared\d3d11\build\AMD_LIB_Minimal_2013.sln
 del /f /q amd_lib\shared\d3d11\build\AMD_LIB_Minimal_2015.sln
 
-del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_2010.sln
-del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_2012.sln
 del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_2013.sln
 del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_2015.sln
 
-del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_Minimal_2010.sln
-del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_Minimal_2012.sln
 del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_Minimal_2013.sln
 del /f /q framework\d3d11\amd_sdk\build\AMD_SDK_Minimal_2015.sln
 
-del /f /q framework\d3d11\dxut\Core\DXUT_2010.sln
-del /f /q framework\d3d11\dxut\Core\DXUT_2012.sln
 del /f /q framework\d3d11\dxut\Core\DXUT_2013.sln
 del /f /q framework\d3d11\dxut\Core\DXUT_2015.sln
 
-del /f /q framework\d3d11\dxut\Optional\DXUTOpt_2010.sln
-del /f /q framework\d3d11\dxut\Optional\DXUTOpt_2012.sln
 del /f /q framework\d3d11\dxut\Optional\DXUTOpt_2013.sln
 del /f /q framework\d3d11\dxut\Optional\DXUTOpt_2015.sln
 goto :EOF
